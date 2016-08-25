645cd25d1b148159ff6d8c528f95cd2a947e1da0
spacemanspiffed <spacemanspiffed23@gmail.com>
2016-08-25 10:20:25 -0600

    initial commit

---
+++ StageApiClient/PostParticipant.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StageApiClient
+{
+	public class PostParticipant
+	{
+		public string[] MonitoringType { get; set; }
+		public string FirstName { get; set; }
+		public string MiddleName { get; set; }
+		public string LastName { get; set; }
+		public string IdNumber { get; set; }
+		public string DateOfBirth { get; set; }
+		public ParticipantHeight Height { get; set; }
+		public ParticipantWeight Weight { get; set; }
+		public string Gender { get; set; }
+		public string CaseNumber { get; set; }
+		public string FileNumber { get; set; }
+		public string ExpectedStartDate { get; set; }
+		public string ExpectedEndDate { get; set; }
+		public string TimeZone { get; set; }
+		public string CourtId { get; set; }
+		public string JudgeId { get; set; }
+		public string Offense { get; set; }
+		public string Occupation { get; set; }
+		public string ParticipantType { get; set; }
+		public string Ethnicity { get; set; }
+	}
+
+	public class ParticipantHeight
+	{
+		public int? Feet { get; set; }
+		public int? Inches { get; set; }
+		public int? Centimeters { get; set; }
+	}
+
+	public class ParticipantWeight
+	{
+		public int? Pounds { get; set; }
+		public int? Kilograms { get; set; }
+	}
+
+}
---
+++ StageApiClient/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StageApiClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CallApi();
+		}
+
+		private static async void CallApi()
+		{
+			var uriString = "https://api.scramstage.com/v1.0/agent/4a132d85-ca8a-4b68-bc8e-5925e05c48e7/participant";
+			Uri stageUri = new System.Uri(uriString);
+			var height = new ParticipantHeight
+			{
+				Feet = 6,
+				Inches = 1
+			};
+			var weight = new ParticipantWeight
+			{
+				Pounds = 111
+			};
+
+			var participant = new PostParticipant
+			{
+				CaseNumber = "123321",
+				CourtId = "1a32d0f9-f532-4faf-97a5-7fba8374ac58",
+				DateOfBirth = "06/28/1982",
+				Ethnicity = "Caucasian",
+				ExpectedStartDate = DateTime.Now.Date.ToString(),
+				ExpectedEndDate = DateTime.Now.Date.AddDays(40).ToString(),
+				FileNumber = "654654",
+				FirstName = "CODE1",
+				MiddleName = "API",
+				LastName = "TEST",
+				Gender = "Male",
+				Weight = weight,
+				Height = height,
+				IdNumber = "1234564565",
+				JudgeId = "07a842a0-dbec-48a5-9886-382d63a7ec84",
+				MonitoringType = new string[] { "CAM" },
+				Occupation = "Worker",
+				Offense = "DUI",
+				ParticipantType = "Parole",
+				TimeZone = "Mountain Standard Time"
+			};
+
+			using (var client = new HttpClient())
+			{
+				client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("AMS_Token", "keyForApiTestProvider");
+				var response = client.PostAsJsonAsync(uriString, participant).Result;
+
+				if (response.IsSuccessStatusCode)
+				{
+					Console.WriteLine("Success" + response.Headers.Location);
+					Console.ReadLine();
+				}
+				else
+				{
+					Console.WriteLine("fail");
+					Console.ReadLine();
+				}
+
+			}
+		}
+	}
+}
